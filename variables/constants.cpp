75d9b6286b41922eb12d473e6b01500d70b11fa9
Priyanshumishra227 <iPriyanshu2207@gmail.com>
2025-11-22 23:32:57 +0530

    Variables is completed here

---
+++ variables/constants.cpp
@@ -0,0 +1,13 @@
+/*
+When you want that no can make any change to 
+the value of any existing variable then you 
+use const keyword with that variable name . 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    const int myAge = 15;
+    cout << myAge;
+}
---
+++ variables/identifiers.cpp
@@ -0,0 +1,17 @@
+/*
+    all variables must be unique names these unique names are called identifiers . 
+
+    identifiers can be short like l , b , r or can be descriptive like length , breadth or radius 
+
+    it is recommended to use descriptive names to make the code more understandable and manageable
+
+    General Rules 
+
+        Names can contain letters, digits and underscores
+        Names must begin with a letter or an underscore (_)
+        Names are case-sensitive (myVar and myvar are different variables) . Acutally the full c++ language is case-sensitive.
+        Names cannot contain whitespaces or special characters like !, #, %, etc.
+        Reserved words (like C++ keywords, such as int) cannot be used as names
+
+*/
+
