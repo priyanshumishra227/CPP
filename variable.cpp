61da773a7464bf46b01b135d2ccca96e589d2573
Priyanshumishra227 <iPriyanshu2207@gmail.com>
2025-11-22 21:46:24 +0530

    There is slight change in comment

--- variable.cpp
+++ variable.cpp
@@ -3,6 +3,6 @@ using namespace std ;
 
 int main(){
     cout<<"This is a variable file in cpp ";
-
+// This is a folder
     return 0;
 }
--- varibale.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std ;
-
-int main(){
-    cout<<"This is a variable file in cpp ";
-
-    return 0;
-}
