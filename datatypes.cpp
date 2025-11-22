e42edf55ed79b8f27bde6394bcf56b6255a5bf03
Priyanshumishra227 <iPriyanshu2207@gmail.com>
2025-11-23 00:15:28 +0530

    Datatypes and userinput files have been added

---
+++ datatypes.cpp
@@ -0,0 +1,20 @@
+// Basic Datatypes declaration and execution has already been covered in variables folder first file 
+
+// In this we will only take auto keyword 
+
+#include <iostream>
+using namespace std;
+int main (){
+
+    // auto detects the datatype by itself 
+    // auto only works if declaration and assignment are done in the sametime  
+
+    auto myNum = 5; // int
+    auto myFloatNum = 5.99f; // float
+    auto myDoubleNum = 9.98; // double
+    auto myLetter = 'D'; // char
+    auto myBoolean = true; // bool
+    auto myString = string("Hello"); // std::string
+
+
+    }
---
+++ userInput.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std ;
+
+int main(){
+
+    int x ;
+    cin >> x;
+    cout << "Your number is "<<x<<endl;
+
+
+    // >> is called extraction operator 
+
+   
+    // simple calculator 
+    
+    int num1 , num2 ;
+
+    cout << "Enter the first number "<<endl;
+    cin >> num1;
+     cout << "Enter the second number "<<endl;
+    cin >> num2;
+
+    cout << num1+num2;
+}
