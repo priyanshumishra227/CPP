c53fd1b1427b97f3e0e5952e7a04fd2485c95022
Priyanshumishra227 <iPriyanshu2207@gmail.com>
2025-11-22 23:09:21 +0530

    Simple Declaration

--- variables/declaringVariable.cpp
+++ variables/declaringVariable.cpp
@@ -3,7 +3,7 @@ using namespace std ;
 
 int main(){
 
-    /*
+/*
     int - stores integers (whole numbers), without decimals, such as 123 or -123
     double - stores floating point numbers, with decimals, such as 19.99 or -19.99
     char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
@@ -13,7 +13,18 @@ int main(){
 
 // syntax 
 
-int num = 21;
-cout << num ;
+int myNum = 5;               // Integer (whole number without decimals)
+double myFloatNum = 5.99;    // Floating point number (with decimals)
+char myLetter = 'D';         // Character
+string myText = "Hello";     // String (text)
+bool myBoolean = true;       // Boolean (true or false)
+
+// we use \n or endl to get the result in new line just as <br> tag in html 
+
+cout<<myNum<<"\n";  
+cout <<myFloatNum<<"\n";
+cout << myLetter<<"\n";
+cout<<myText<<"\n";
+cout<<myBoolean<<"\n";
 
 }
