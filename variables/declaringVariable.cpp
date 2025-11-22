62ef389bbf9d750415c2a496d0a9d0da3b050129
Priyanshumishra227 <iPriyanshu2207@gmail.com>
2025-11-22 23:16:33 +0530

    Simple Declaration

--- variables/declaringVariable.cpp
+++ variables/declaringVariable.cpp
@@ -11,7 +11,7 @@ int main(){
     bool - stores values with two states: true or false
 */
 
-// syntax 
+// we can do both declare and assign value to variable together like in below examples or check line 45 
 
 int myNum = 5;               // Integer (whole number without decimals)
 double myFloatNum = 5.99;    // Floating point number (with decimals)
@@ -26,5 +26,26 @@ cout <<myFloatNum<<"\n";
 cout << myLetter<<"\n";
 cout<<myText<<"\n";
 cout<<myBoolean<<"\n";
+
+
+// Multiple varibles 
+
+// declare many variables 
+
+int a = 5 , b = 3 , c = 4;
+cout << a <<endl;
+cout << b <<endl;
+cout << c <<endl;
+
+
+// one value to many variables 
+
+
+
+int x ,y,z;  // we can declare variables without giving values and then give values to them later 
+x = y= z = 10;
+cout << x <<endl;
+cout << y <<endl;
+cout << z <<endl;
 
 }
