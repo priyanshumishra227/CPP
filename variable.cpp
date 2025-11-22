81e84d45dc06bca645d432ae5944e3a1b08d5f47
Priyanshumishra227 <iPriyanshu2207@gmail.com>
2025-11-22 23:46:03 +0530

    Example has been solved

--- variable.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std ;
-
-int main(){
-    cout<<"This is a variable file in cpp ";
-// This is a folder
-    return 0;
-}
---
+++ variables/example.cpp
@@ -0,0 +1,18 @@
+// This will calculate the area of circle 
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    const double pi = 3.14 ; 
+
+    int radius = 3;
+
+    double area = pi*radius ; //two variable values can added , multiplied , subtracted and divided amongst eachother 
+
+    cout << area;
+
+    return 0;
+}
