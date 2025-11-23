2f93b58f125fc65d72b19c78f6123eba32e88e7b
Priyanshumishra227 <iPriyanshu2207@gmail.com>
2025-11-23 17:33:09 +0530

    Comparision and arithemetic

---
+++ operators/arithemetic.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // common maths operators like + , - , * , / , %(remainder or modols) , ++ (increment) , -- (decrement)
+    
+    int x = 10;
+    int y = 3;
+
+    cout << (x + y) << "\n"; // 13
+    cout << (x - y) << "\n"; // 7
+    cout << (x * y) << "\n"; // 30
+    cout << (x / y) << "\n"; // 3 (integer division) for decimal part declare varibale as float or double
+    cout << (x % y) << "\n"; // 1
+
+    int z = 5;
+    ++z;
+    cout << z << "\n"; // 6
+    --z;
+    cout << z << "\n"; // 5
+
+
+    return 0;
+}
---
+++ operators/comparision.cpp
@@ -0,0 +1,12 @@
+/*
+
+    They return boolean values either true or false i.e. 1 or 0
+    operator name                   example
+    ==	Equal to	                x == y	
+    !=	Not equal	                x != y	
+    >	Greater than	            x > y	
+    <	Less than	                x < y	
+    >=	Greater than or equal to	x >= y	
+    <=	Less than or equal to	    x <= y	
+
+*/
